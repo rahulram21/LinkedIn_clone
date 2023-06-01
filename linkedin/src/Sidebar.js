59f8856afa93d27e3ab608fda3be3af8c1b1e2ef
rahulram21 <rahulkannom21@gmail.com>
2023-06-01 08:50:07 +0530

    updated the sidebar css style but the alignment is not proper yet

--- linkedin/src/Sidebar.js
+++ linkedin/src/Sidebar.js
@@ -3,6 +3,12 @@ import './Sidebar.css';
 import { Avatar } from '@mui/material';
 
 function Sidebar() {
+  const recentItem = (topic) =>(
+    <div className='sidebar__recentItem'>
+      <span className='sidebar__hash'>#</span>
+      <p>{topic}</p>
+    </div>
+  )
   return (
     <div className='sidebar'>
         <div className='sidebar__top'>
@@ -19,11 +25,13 @@ function Sidebar() {
           </div>
           <div className='sidebar__stat'>
             <p>Views on post</p>
-            <p className='sidebar__statnum'>300000</p>
+            <p className='sidebar__statnum'>3000</p>
           </div>
         </div>
         <div className='sidebar__bottom'>
           <p>Recent</p>
+          {recentItem('reactJs')}
+          {recentItem('software')}
         </div>
     </div>
   )
